02bfe23f52e6aa9b1caa5a97c4c53c64d2bd299c
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-09 15:32:26 +0100

    Require Node.js 8, add TypeScript definition

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 import path from 'path';
 import test from 'ava';
-import m from './';
+import npmRunPath from '.';
 
-test(t => {
+test('npmRunPath', t => {
 	t.is(
-		m({path: ''}).split(path.delimiter)[0],
+		npmRunPath({path: ''}).split(path.delimiter)[0],
 		path.join(__dirname, 'node_modules/.bin')
 	);
 	t.is(
-		m.env({env: {PATH: 'foo'}}).PATH.split(path.delimiter)[0],
+		npmRunPath.env({env: {PATH: 'foo'}}).PATH.split(path.delimiter)[0],
 		path.join(__dirname, 'node_modules/.bin')
 	);
 });
